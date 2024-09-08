6391a51756a5ffa7cf1e5ca42b5326e9b51b64da
Math_Still <1742996732@qq.com>
2024-09-08 11:25:15 +0800

    fix: 修复全屏无法点击控件的bug

--- viewapp/static/index.js
+++ viewapp/static/index.js
@@ -1,67 +1,86 @@
 const dynamicDiv = document.getElementById('dynamicDiv');
 const autoselect = document.getElementById('autoselect');
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
-
+const body = document.body;
+let timeoutIds = [];
 document.addEventListener('DOMContentLoaded', function () {
 
     dynamicDiv.onclick = function () {
-        init()
-        dynamicDiv.style.backgroundColor = "#FFFFFF"
-        if (dynamicDiv.requestFullscreen) {
-            dynamicDiv.requestFullscreen();
-        } else if (dynamicDiv.mozRequestFullScreen) { // Firefox
-            dynamicDiv.mozRequestFullScreen();
-        } else if (dynamicDiv.webkitRequestFullscreen) { // Chrome, Safari and Opera
-            dynamicDiv.webkitRequestFullscreen();
-        } else if (dynamicDiv.msRequestFullscreen) { // IE/Edge
-            dynamicDiv.msRequestFullscreen();
+        body.style.backgroundColor = "#FFFFFF"
+        if (body.requestFullscreen) {
+            body.requestFullscreen();
+        } else if (body.mozRequestFullScreen) { // Firefox
+            body.mozRequestFullScreen();
+        } else if (body.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            body.webkitRequestFullscreen();
+        } else if (body.msRequestFullscreen) { // IE/Edge
+            body.msRequestFullscreen();
         }
-        start()
     };
-    
+
 });
+function delay(func, timeset) {
+    const timeoutId = setTimeout(func, timeset);
+    timeoutIds.push(timeoutId);
+}
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.sel').forEach(function (element) {
-        element.addEventListener('click', function (event){
+        element.addEventListener('click', function (event) {
             if (event.target.innerText === "实验1") {
                 exp()
                 your_function()
-                setTimeout(start, 2000);
+                delay(start, 2000);
                 // start()
             } else if (event.target.innerText === "实验2") {
                 exp()
                 your_function()
-                setTimeout(start, 2000);
+                delay(start, 2000);
             } else if (event.target.innerText === "实验3") {
                 exp()
                 your_function()
-                setTimeout(start, 2000);
+                delay(start, 2000);
             } else if (event.target.innerText === "实验4") {
                 exp()
                 your_function()
-                setTimeout(start, 2000);
+                delay(start, 2000);
             } else if (event.target.innerText === "实验5") {
                 exp()
                 your_function()
-                setTimeout(start, 2000);
+                delay(start, 2000);
+
             }
         });
     });
 });
-function your_function(){
-    
+function your_function() {
+
 }
+document.addEventListener('fullscreenchange', function () {
+    if (document.fullscreenElement) {
+        start()
+    } else {
+        init()
+    }
+});
 function init() {
+    timeoutIds.forEach(clearTimeout);
+    timeoutIds = []; 
     dynamicDiv.style.display = 'block'
     autoselect.style.display = 'none'
+    // autoselect.style.zIndex = 0
+    // dynamicDiv.style.zIndex = 999
 }
 function start() {
     dynamicDiv.style.display = 'none'
     autoselect.style.display = 'block'
+    // autoselect.style.zIndex = 999
+    // dynamicDiv.style.zIndex = 0
+
 }
-function exp(){
+function exp() {
     dynamicDiv.style.display = 'none'
     autoselect.style.display = 'none'
+    // autoselect.style.zIndex = 999
+    // dynamicDiv.style.zIndex = 999
 }
 function fetchData() {
     fetch(`http://127.0.0.1:8000/getdata`)
